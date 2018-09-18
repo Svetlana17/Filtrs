21d4ef6e38026b6aeb4ba4dca075366a62689d93
Users <lana.chebotareva.95@mail.ru>
2018-09-18 09:27:18 +0300

    Add six lines for accelerometr

--- app/src/main/java/com/example/user/magnitometr/RunActivity.java
+++ app/src/main/java/com/example/user/magnitometr/RunActivity.java
@@ -2,7 +2,10 @@ package com.example.user.magnitometr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,73 +17,250 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class RunActivity extends AppCompatActivity implements View.OnClickListener{
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+public class RunActivity extends AppCompatActivity
+        implements View.OnClickListener,
+ SensorEventListener{
+
+    String[] data = {"Ось ох", "Ось оу", "Ось oz"};
+    Button button;
     private SensorManager mSensorManager;
+
     Sensor sensorAccelerometr;
-    Button mButton;
-    Spinner spinner;
-    String[] acxios = {"ускорение  по х", "ускорение по y ", "ускорение по z "};
+    GraphView graph;
+    private double graph2LastXValue = 5d;
+    private double graph2LastYValue = 5d;
+    private double graph2LastZValue = 5d;
+    private Double[] dataPoints;
+    LineGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> seriesX;
+    LineGraphSeries<DataPoint> seriesZ;
+    LineGraphSeries<DataPoint> seriesXX;
+    LineGraphSeries<DataPoint> seriesYY;
+    LineGraphSeries<DataPoint> seriesZZ;
+    private Thread thread;
+    private boolean plotData = true;
+    float xx;
+    float yy;
+    float zz;
+    private boolean graficflag = false;
+    private float On_1 = 1;
+    private float altha = 0.1f;
+    private boolean state;
+    private int timer=0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
-        spinner = (Spinner)findViewById(R.id.spinner);
-
+        button = findViewById(R.id.button);
+        state = false;
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensorAccelerometr = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, acxios);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
-        ////
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item =  parent.getItemAtPosition(position).toString();
-                System.out.println(item);
-                switch (item){
-                    case "ускорение  по х":
-//                      graficflag = true;
-                        Toast toast = Toast.makeText(getApplicationContext(),
-                                "Ваш выбор: " + item, Toast.LENGTH_LONG);
-                        toast.show();
-                        System.out.println(item + " " + id);
-                        break;
-                    case "ускорение по y ":
-//                      graficflag = false;
-                        Toast toasts = Toast.makeText(getApplicationContext(),
-                                "Ваш выбор: " + item, Toast.LENGTH_LONG);
-                        toasts.show();
-                        System.out.println(item + " " + id);
-                        break;
-                    case "ускорение по z ":
-//                      graficflag = true;
-                        Toast toast1 = Toast.makeText(getApplicationContext(),
-                                "Ваш выбор: " + item, Toast.LENGTH_LONG);
-                        toast1.show();
-                        System.out.println(item + " " + id);
-                        break;
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 0),
+        });
+        series.setColor(Color.GREEN);
+        graph.addSeries(series);
+        seriesX = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 0),
 
+        });
+        seriesX.setColor(Color.BLACK);
+
+        seriesZ = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 0),
+        });
+        seriesZ.setColor(Color.RED);
+        seriesXX = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+
+    });
+        seriesXX.setColor(Color.YELLOW);
+//
+    seriesZZ = new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0, 0),
+    });
+        seriesZZ.setColor(Color.LTGRAY);
+
+//
+    seriesYY=new LineGraphSeries<DataPoint>(new DataPoint[]{
+        new DataPoint(0,0),
+    });
+        seriesYY.setColor(Color.MAGENTA);
+
+        graph.addSeries(seriesX);
+        graph.addSeries(series);
+        graph.addSeries(seriesZ);
+        graph.addSeries(seriesXX);
+        graph.addSeries(seriesYY);
+        graph.addSeries(seriesZZ);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
+        feedMultiple();
+
+        }
+//}
+        public void addEntry(SensorEvent event){
+            float[] values=event.values;
+            float x=values[0];
+            System.out.println(x);
+            float y=values[1];
+            System.out.println(y);
+            float z=values[2];
+            System.out.println(z);
+
+            graph2LastXValue +=1d;
+            graph2LastYValue +=1d;
+            graph2LastZValue +=1d;
+            xx = (float) (On_1 + altha * (x - On_1));
+            yy = (float) (On_1 + altha * (y - On_1));
+            zz = (float) (On_1 + altha * (z - On_1));
+
+            series.appendData(new DataPoint(graph2LastYValue, y), true, 20);
+            seriesX.appendData(new DataPoint(graph2LastXValue, x), true, 20);
+            seriesZ.appendData(new DataPoint(graph2LastZValue, z), true, 20);
+            seriesXX.appendData(new DataPoint(graph2LastXValue,xx), true,20);
+            seriesYY.appendData(new DataPoint(graph2LastYValue,yy),true,20);
+            seriesZZ.appendData(new DataPoint(graph2LastZValue,zz),true,20);
+            graph.addSeries(series);
+            graph.addSeries(seriesX);
+            graph.addSeries(seriesZ);
+            graph.addSeries(seriesXX);
+            graph.addSeries(seriesYY);
+            graph.addSeries(seriesZZ);
+}
+private  void addDataPoint(double acceleration){
+        dataPoints[499]=acceleration;
+}
+    private void feedMultiple() {
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    plotData = true;
+                    try {
+                        Thread.sleep(500);
+                    }
+                    catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (thread != null) {
+            thread.interrupt();
+        }
+        mSensorManager.unregisterListener((SensorEventListener) this);
+
+    }
+
+//    @Override
+    public void onSensorChanged(final SensorEvent event) {
+        if (plotData) {
+//            addEntry(event);
+            //
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry(event);
+                        }
+                    });
 
 
-        mButton=(Button) findViewById(R.id.button);
-        mButton.setOnClickListener((View.OnClickListener) this);
+                }
+
+            }).start();
+
+            //
+            plotData = false;
+        }
+    }
+
+//    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener((SensorEventListener) this, sensorAccelerometr, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mSensorManager.unregisterListener((SensorEventListener) RunActivity.this);
+        thread.interrupt();
+        super.onDestroy();
     }
 
 
 
-    public void onClick(View v)
-    {
-        Intent intent=new Intent();
+
+
+//        // адаптер
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//        spinner.setAdapter(adapter);
+//        // заголовок
+//        spinner.setPrompt("Title");
+//        // выделяем элемент
+//        spinner.setSelection(2);
+//        // устанавливаем обработчик нажатия
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view,
+//                                       int position, long id) {
+//                // показываем позиция нажатого элемента
+//                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//            }
+//        });
+//
+//
+ public void onClick(View v) {
+        Intent intent = new Intent();
         intent.setClass(this, MainActivity.class);
 
         startActivity(intent);
         finish();
     }
-}
+
+
+     }
+//}
